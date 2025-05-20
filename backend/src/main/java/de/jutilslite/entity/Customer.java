a8573703099d27b648e4e04948a717ea106688d3
dev-json <hsvbuh12@gmail.com>
2025-05-20 19:17:40 +0200

    start(project): inital-commit

---
+++ backend/src/main/java/de/jutilslite/entity/Customer.java
@@ -0,0 +1,18 @@
+package de.jutilslite.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+}
