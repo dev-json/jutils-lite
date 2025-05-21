7b551091bb6ce5e5950b8982946c0d00c6588f3c
dev-json <hsvbuh12@gmail.com>
2025-05-21 21:32:14 +0200

    feat(customers): adding customer base

--- backend/src/main/java/de/jutilslite/entity/Customer.java
+++ backend/src/main/java/de/jutilslite/entity/Customer.java
@@ -3,7 +3,9 @@ package de.jutilslite.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Data;
 
+@Data
 @Entity
 public class Customer {
 
